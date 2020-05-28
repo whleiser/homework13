b7293b3fcb3e36a923206712cf29edcd30ece453
Hunter Leiser <hunterleiser@MacBook-Pro-2020.local>
2020-05-28 12:34:05 -0500

    updated db

--- Solved/config/connection.js
+++ Solved/config/connection.js
@@ -1,21 +1,26 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "cat_db"
-});
+var connection;
+if (process.env.JAWSDB_GRAY_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_GRAY_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "",
+        database: "cat_db",
+    });
+}
 
 // Make connection.
 connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
 });
 
 // Export connection for our ORM to use.
